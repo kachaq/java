8fabeeecb685fd5cbee67ebba4049d7120d42f6e
Yusuf Butun <arbutun@gmail.com>
2021-08-29 03:51:41 -0400

    nLayered Done!

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -3,6 +3,7 @@ package nLayeredDemo;
 
 import nLayeredDemo.business.abstracts.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
 //import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
 import nLayeredDemo.dataAccess.concretes.NewPackageProductDao;
 import nLayeredDemo.entities.concretes.Product;
@@ -14,7 +15,9 @@ public class Main {
 		// Bad Code
 		// Spring IoC use!
 		//ProductService productService = new ProductManager(new HibernateProductDao());
-		ProductService productService = new ProductManager(new NewPackageProductDao());
+		ProductService productService = new ProductManager(new NewPackageProductDao()
+				,new JLoggerManagerAdapter());
+		
 		
 		// Change second 1 to 2
 		Product product = new Product(1, 2, "Apple", 12,50);
---
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -0,0 +1,8 @@
+// for simulation purpose do not touch
+package nLayeredDemo.jLogger;
+
+public class JLoggerManager {
+	public void log(String message) {
+		System.out.println("Logged with JLogger.");
+	}
+}

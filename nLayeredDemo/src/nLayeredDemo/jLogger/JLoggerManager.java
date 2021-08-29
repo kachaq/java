2880bcc793649401282fc65957f55de0e6f43dab
Yusuf Butun <arbutun@gmail.com>
2021-08-29 03:58:38 -0400

    fixed

--- nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -3,6 +3,6 @@ package nLayeredDemo.jLogger;
 
 public class JLoggerManager {
 	public void log(String message) {
-		System.out.println("Logged with JLogger.");
+		System.out.println("Logged with JLogger." + message);
 	}
 }
